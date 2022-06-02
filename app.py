a0d94bf2f71425737e65971e4394d2bea1bf854a
Padma S <you@example.com>
2022-06-02 09:35:59 +0100

    Added Postgres Horoku linl DB

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import Api
@@ -13,7 +14,21 @@ app = Flask(__name__)
 
 app.config['DEBUG']=True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DataBase_URL','sqlite:///data.db')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DataBase_URL','sqlite:///data.db')
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+print(uri)
+if uri == None:
+    uri = "postgresql://"
+else:
+#uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
